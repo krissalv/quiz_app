c1a18fbe74f3ef81641fc8214b00a5e16a07e40c
Kristin Salvacion <nitnitks@gmail.com>
2025-02-25 12:05:21 -0500

    Created Question Models and data

---
+++ lib/data/questions.dart
@@ -0,0 +1,49 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'How did Ed lose his arm in FMA', 
+    [
+      'Putting his brother\'s soul in a suit of armor', 
+      'In a freak cooking accident', 
+      'Fighting the MEG alongside Jason Statham', 
+      'This never happened'
+    ]
+  ),
+  QuizQuestion(
+    'What city is Scar from?', 
+    [
+      'Ishbal', 
+      'Central City', 
+      'Dublith', 
+      'San Antonio'
+    ]
+  ),
+  QuizQuestion(
+    'How many times did Naruto fail the graduation test?', 
+    [
+      '1',
+      '2',
+      '3',
+      '4'
+    ]
+  ),
+  QuizQuestion(
+    'How many seasons does one piece have?', 
+    [
+      '5',
+      '20',
+      '59',
+      '123'
+    ]
+  ),
+  QuizQuestion(
+    'What studio created your lie in april', 
+    [
+      'Kyoto Studio', 
+      'MAPPA', 
+      'A1', 
+      'Aniplex'
+    ]
+  ),
+]
